906fa53e518288072445cede02f1a04691ff30e9
CrispenGari <Crispengari@gmail.com>
2021-07-18 02:00:39 +0200

    fleet-view-resposive

--- src/Components/FleetViewer/Center/Center.jsx
+++ src/Components/FleetViewer/Center/Center.jsx
@@ -3,6 +3,8 @@ import "./Center.css";
 import { IoIosShareAlt } from "react-icons/io";
 import { IconButton } from "@material-ui/core";
 import { FavoriteBorder, Favorite, GetApp } from "@material-ui/icons";
+import { MdDelete } from "react-icons/md";
+
 const Center = () => {
   return (
     <div className="center">
@@ -10,6 +12,10 @@ const Center = () => {
         src="https://firebasestorage.googleapis.com/v0/b/likeme-a104d.appspot.com/o/fleets%2Ff612f439-91b6-4a7d-9891-f21cadf12800_SSxRdkrmtBSTpEkKjjQUA6o1BWD2?alt=media&token=10182699-a1b0-404b-8f9d-d45e59049bfb"
         alt=""
       />
+      <p className="center__caption">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et fuga quo
+        sint suscipit provident nostrum recusandae non rem facilis molestiae?
+      </p>
       <div className="center__controls">
         <div>
           <IconButton title="share">
@@ -22,6 +28,11 @@ const Center = () => {
             <GetApp className="center__controls__icon__download" />
           </IconButton>
           <div className="center__controls__stats">4</div>
+        </div>
+        <div>
+          <IconButton title="delete">
+            <MdDelete className="center__controls__icon__delete" />
+          </IconButton>
         </div>
         <div>
           <IconButton title="react">
--- src/Components/FleetViewer/Input/Input.jsx
+++ src/Components/FleetViewer/Input/Input.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import "./Input.css";
 const Input = () => {
   return (
-    <div className="input">
-      <h1>input</h1>
-    </div>
+    <form className="fleet__viewer__input">
+      <input type="text" placeholder="reply to this fleet @username..." />
+      <button>send</button>
+    </form>
   );
 };
 
