c41c5fac39b81899c93c7739395fb2aae10ee72a
CrispenGari <Crispengari@gmail.com>
2021-07-22 22:59:36 +0200

    profile-posts-nav-updated

--- src/Components/ProfilePostsNav/ProfilePostsNav.jsx
+++ src/Components/ProfilePostsNav/ProfilePostsNav.jsx
@@ -1,13 +1,15 @@
 import "./ProfilePostsNav.css";
 import React from "react";
 import { IoMdPhotos } from "react-icons/io";
-import { GiSelfLove } from "react-icons/gi";
-import { CgHashtag } from "react-icons/cg";
+import { GiSelfLove, GiKnightBanner } from "react-icons/gi";
+import { CgHashtag, CgProfile } from "react-icons/cg";
 import ProfilePostsNavItem from "../ProfilePostsNavItem/ProfilePostsNavItem";
 const ProfilePostNav = () => {
   return (
     <div className="profile__posts__nav">
       <ProfilePostsNavItem active title="photos" Icon={IoMdPhotos} />
+      <ProfilePostsNavItem title="profiles" Icon={CgProfile} />
+      <ProfilePostsNavItem title="covers" Icon={GiKnightBanner} />
       <ProfilePostsNavItem title="most liked" Icon={GiSelfLove} />
       <ProfilePostsNavItem title="tags" Icon={CgHashtag} />
     </div>
