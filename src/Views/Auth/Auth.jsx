cc9fa92a7467cda0e37c8273297b86e6c526d93d
CrispenGari <Crispengari@gmail.com>
2021-07-23 16:50:08 +0200

    login-auth-logo

--- src/Views/Auth/Auth.jsx
+++ src/Views/Auth/Auth.jsx
@@ -5,13 +5,17 @@ import Login from "../../Components/Login/Login";
 import Profile from "../../Components/Profile/Profile";
 import Reset from "../../Components/Reset/Reset";
 
+import { logos } from "../../utils/logos";
+
 const Auth = () => {
   document.title = `LikeMe • Authentication`;
   const [cardToMount, setCardToMount] = useState("login");
   const [credentials, setCredentials] = useState({});
   return (
     <div className="auth">
-      <h1>LIKE ME</h1>
+      {/* {<h1>LIKE ME</h1>} */}
+
+      <img src={logos.main_logo} alt="" />
       <div className="auth__main">
         {cardToMount === "register" ? (
           <Register
