f04f161dcd77777a84bc676b9faeac568d9d1d5e
CrispenGari <Crispengari@gmail.com>
2021-07-24 21:01:33 +0200

    fleets-nav

--- src/Components/Fleets/Fleets.jsx
+++ src/Components/Fleets/Fleets.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import "./Fleets.css";
 import { Fleet } from "../../Components";
 import { useSelector } from "react-redux";
+import { IconButton } from "@material-ui/core";
 import FleetViewer from "../FleetViewer/FleetViewer";
-
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 const Fleets = ({ setFleetImage, title }) => {
   const user = useSelector((state) => state.user);
   const users = useSelector((state) => state.users)?.filter(
@@ -16,7 +17,21 @@ const Fleets = ({ setFleetImage, title }) => {
   );
 
   const [displayName, setDisplayName] = useState("");
-
+  const scrollRef = React.useRef(null);
+  const navigateLeft = () => {
+    scrollRef.current.scrollBy({
+      top: 0,
+      left: -100,
+      behavior: "smooth",
+    });
+  };
+  const navigateRight = () => {
+    scrollRef.current.scrollBy({
+      top: 0,
+      left: 100,
+      behavior: "smooth",
+    });
+  };
   return (
     <>
       {title ? <p className="fleets__header">{title}</p> : null}
@@ -30,7 +45,15 @@ const Fleets = ({ setFleetImage, title }) => {
             setDisplayName={setDisplayName}
           />
         ) : null}
-        <div className="fleets__container">
+        <IconButton
+          className={`fleets__nav__button__left ${
+            title ? "" : "fleets__nav__button__left__no--title"
+          }`}
+          onClick={navigateLeft}
+        >
+          <BiChevronLeft />
+        </IconButton>
+        <div className="fleets__container" ref={scrollRef}>
           {users?.map((_user) => (
             <Fleet
               key={_user?.id}
@@ -39,7 +62,14 @@ const Fleets = ({ setFleetImage, title }) => {
             />
           ))}
         </div>
+        <IconButton
+          className="fleets__nav__button__right"
+          onClick={navigateRight}
+        >
+          <BiChevronRight />
+        </IconButton>
       </div>
+
       <FleetViewer displayName={displayName} setDisplayName={setDisplayName} />
     </>
   );
